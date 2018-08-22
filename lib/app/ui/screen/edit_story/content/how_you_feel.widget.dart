78fc2135fe425b6f1f7b11ee99cc3ee1de2f147d
yohom <382146139@qq.com>
2018-08-22 17:28:43 +0800

    1. how you feel的动画完成

--- lib/app/ui/screen/edit_story/content/how_you_feel.widget.dart
+++ lib/app/ui/screen/edit_story/content/how_you_feel.widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:memoir/app/model/bean/page_change.dart';
 import 'package:memoir/app/res/drawable.dart';
+import 'package:memoir/app/ui/widget/fonted_text.dart';
 import 'package:memoir/app/ui/widget/question.widget.dart';
 import 'package:memoir/app/ui/widget/step_indicator.widget.dart';
 import 'package:memoir/framework/res.dart';
@@ -30,17 +31,17 @@ class _Feel extends StatefulWidget {
 }
 
 class _FeelState extends State<_Feel> {
-  final _controller = PageController(viewportFraction: 0.3);
+  final _controller = PageController(viewportFraction: 0.35);
   final _iconDataList = [
-    ReflectlyIcons.happy,
-    ReflectlyIcons.blessed,
-    ReflectlyIcons.lucky,
-    ReflectlyIcons.good,
-    ReflectlyIcons.confused,
-    ReflectlyIcons.stressed,
-    ReflectlyIcons.angry,
-    ReflectlyIcons.anxious,
-    ReflectlyIcons.down,
+    {ReflectlyIcons.happy: 'Happy'},
+    {ReflectlyIcons.blessed: 'Blessed'},
+    {ReflectlyIcons.lucky: 'Lucky'},
+    {ReflectlyIcons.good: 'Good'},
+    {ReflectlyIcons.confused: 'Confused'},
+    {ReflectlyIcons.stressed: 'Stressed'},
+    {ReflectlyIcons.angry: 'Angry'},
+    {ReflectlyIcons.anxious: 'Anxious'},
+    {ReflectlyIcons.down: 'Down'},
   ];
 
   @override
@@ -50,11 +51,50 @@ class _FeelState extends State<_Feel> {
 
   @override
   Widget build(BuildContext context) {
-    return Carousel.builder(
-      viewportFraction: 0.3,
+    return PageView.builder(
+      controller: _controller,
       itemCount: _iconDataList.length,
-      itemBuilder: (_, index) {
-        return Icon(_iconDataList[index], size: 80.0, color: Colors.white);
+      itemBuilder: (context, index) {
+        return AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            double iconFactor = 1.0;
+            double labelFactor = 1.0;
+            if (_controller.position.haveDimensions) {
+              iconFactor = _controller.page - index;
+              iconFactor = (1 - (iconFactor.abs() * .6)).clamp(0.4, 1.0);
+
+              labelFactor = _controller.page - index;
+              labelFactor = (1 - labelFactor.abs()).clamp(0.0, 1.0);
+            } else if (index == 1) {
+              // 如果是首次加载, 由于haveDimensions为false, 会导致index为1处的item
+              // 的value也会是1, 这里手动调整一下value.
+              iconFactor = 0.5;
+              labelFactor = 0.0;
+            }
+
+            return Transform.scale(
+              scale: Curves.easeOut.transform(iconFactor),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    _iconDataList[index].keys.toList()[0],
+                    size: 80.0,
+                    color: Colors.white.withOpacity(iconFactor),
+                  ),
+                  SPACE_BIG,
+                  QuicksandText(
+                    _iconDataList[index].values.toList()[0],
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(labelFactor),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
       },
     );
   }
